063159024deb2ee1ce44f553f65866403e4f982d
reformnst <dickyindraofficial@gmail.com>
2019-09-09 01:55:34 +0700

    adding storybook action addon

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { storiesOf } from "@storybook/react"
 import { withKnobs, text, boolean, select } from "@storybook/addon-knobs"
+import { action } from "@storybook/addon-actions"
 
 import Button from "./button"
 
@@ -20,55 +21,59 @@ const stories = storiesOf("Button", module)
 
 stories.addDecorator(withKnobs)
 
-stories.add("Default", () => <Button disabled={boolean("Disabled", false)}>{text("Label", "Default Button")}</Button>)
+stories.add("Default", () => (
+    <Button disabled={boolean("Disabled", false)} onClick={action("clicked")}>
+        {text("Label", "Default Button")}
+    </Button>
+))
 stories.add("Primary", () => (
-    <Button variant={variantOptions.Primary} disabled={boolean("Disabled", false)}>
+    <Button variant={variantOptions.Primary} disabled={boolean("Disabled", false)} onClick={action("clicked")}>
         {text("Label", "Primary Button")}
     </Button>
 ))
 stories.add("Secondary", () => (
-    <Button variant={variantOptions.Secondary} disabled={boolean("Disabled", false)}>
+    <Button variant={variantOptions.Secondary} disabled={boolean("Disabled", false)} onClick={action("clicked")}>
         {text("Label", "Secondary Button")}
     </Button>
 ))
 stories.add("Success", () => (
-    <Button variant={variantOptions.Success} disabled={boolean("Disabled", false)}>
+    <Button variant={variantOptions.Success} disabled={boolean("Disabled", false)} onClick={action("clicked")}>
         {text("Label", "Success Button")}
     </Button>
 ))
 stories.add("Danger", () => (
-    <Button variant={variantOptions.Danger} disabled={boolean("Disabled", false)}>
+    <Button variant={variantOptions.Danger} disabled={boolean("Disabled", false)} onClick={action("clicked")}>
         {text("Label", "Danger Button")}
     </Button>
 ))
 stories.add("Warning", () => (
-    <Button variant={variantOptions.Warning} disabled={boolean("Disabled", false)}>
+    <Button variant={variantOptions.Warning} disabled={boolean("Disabled", false)} onClick={action("clicked")}>
         {text("Label", "Warning Button")}
     </Button>
 ))
 stories.add("Info", () => (
-    <Button variant={variantOptions.Info} disabled={boolean("Disabled", false)}>
+    <Button variant={variantOptions.Info} disabled={boolean("Disabled", false)} onClick={action("clicked")}>
         {text("Label", "Info Button")}
     </Button>
 ))
 stories.add("Light", () => (
-    <Button variant={variantOptions.Light} disabled={boolean("Disabled", false)}>
+    <Button variant={variantOptions.Light} disabled={boolean("Disabled", false)} onClick={action("clicked")}>
         {text("Label", "Light Button")}
     </Button>
 ))
 stories.add("Dark", () => (
-    <Button variant={variantOptions.Dark} disabled={boolean("Disabled", false)}>
+    <Button variant={variantOptions.Dark} disabled={boolean("Disabled", false)} onClick={action("clicked")}>
         {text("Label", "Dark Button")}
     </Button>
 ))
 stories.add("Link", () => (
-    <Button variant={variantOptions.Link} disabled={boolean("Disabled", false)}>
+    <Button variant={variantOptions.Link} disabled={boolean("Disabled", false)} onClick={action("clicked")}>
         {text("Label", "Link Button")}
     </Button>
 ))
 
 stories.add("Disabled", () => (
-    <Button disabled={true} variant={select("Variant", variantOptions, variantOptions.Primary)}>
+    <Button disabled={true} variant={select("Variant", variantOptions, variantOptions.Primary)} onClick={action("clicked")}>
         {text("Label", "Disabled Button")}
     </Button>
 ))
